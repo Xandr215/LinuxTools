5c68b78e998989660a8c756f34ea4bed1b508797
Xandr215 <xandr215-github@outlook.com>
2024-04-21 10:52:37 -0500

    Update instpgpt.sh

--- dedicated/appinst/instpgpt.sh
+++ dedicated/appinst/instpgpt.sh
@@ -1,17 +1,24 @@
+path_bashrc() {
+    local line_to_add='export PATH="$HOME/.local/bin:$PATH"'
+    if ! grep -qxF "$line_to_add" ~/.bashrc; then
+        # Append the line to ~/.bashrc
+        echo "$line_to_add" >> ~/.bashrc
+        echo "PATH line added to ~/.bashrc."
+        # Source ~/.bashrc to apply changes immediately
+        source ~/.bashrc
+        echo "Changes applied."
+    fi
+}
 sudo apt-get update -y
 sudo apt-get full-upgrade -y
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install python3.11 python3.11-venv -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 110
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 100
 sudo update-alternatives --config python3
 sudo apt-get install python3-pip -y
+path_bashrc
 pip install poetry
-echo Add path in ~/.bashrc to get rid of Pip path errors, if this is not already done. The path is export PATH="$HOME/.local/bin:$PATH"
-wait 10
-nano ~/.bashrc
-source .bashrc
-echo $PATH
 poetry --version
 git clone https://github.com/imartinez/privateGPT
 cd privateGPT
