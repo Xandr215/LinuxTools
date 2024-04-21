7e13fd97731ae0f47013ecfd44d5903cbcdf2132
Xandr215 <xandr215-github@outlook.com>
2024-04-21 14:16:26 -0500

    Add files via upload

---
+++ dedicated/appinst/instnode.sh
@@ -0,0 +1,4 @@
+sudo apt-get install ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get install nodejs
